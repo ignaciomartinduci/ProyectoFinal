c0e89a2f300fba06dbea575cd43578b8cdb78dae
ignaciomartinduci8 <ignacio8martin@gmail.com>
2026-05-03 11:17:49 -0300

    Flags listas para correr

--- flags.m
+++ flags.m
@@ -4,9 +4,9 @@ TEST_LOOP_IK = 0; % Requiere habilitar la anterior, testea en loop posiciones al
 PRINT_SOLUTIONS = 0; % Imprime soluciones obtenidas, geométricas y periódicas.
 TEST_WORKSPACE = 0; % Prueba del espacio de trabajo
 SINGULARITIES = 0; % Busca y verifica puntos singulares
-TEST_GEN_TRAJ = 1; % Prueba ejemplo de la generación de trayectorias
-TAREA = 0; % Tarea principal
+TEST_GEN_TRAJ = 0; % Prueba ejemplo de la generación de trayectorias
+TAREA = 1; % Tarea principal
 IK_COMPARISON = 0; % Visualización de periodicidad articular de soluciones IK
 
 %% Flags de opciones
-CAD_DESIGN = 1; % Diseño 3D: 1 = models/CAD  |  2 = models/CAD2
+CAD_DESIGN = 2; % Diseño 3D: 1 = models/CAD  |  2 = models/CAD2
